169fb0ebcaa94ef97aeda2b00aa45f45fbeb1458
Peter Donald <peter@realityforge.org>
2014-01-13 09:09:48 +1100

    Import the adapter classes from the replicant library

---
+++ src/main/java/org/realityforge/gwt/datatypes/client/collections/JsoReadOnlySet.java
@@ -0,0 +1,69 @@
+package org.realityforge.gwt.datatypes.client.collections;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * An adapter converting the JsArray to support a Set interface.
+ */
+public final class JsoReadOnlySet<E extends JavaScriptObject>
+  extends AbstractSet<E>
+  implements Set<E>
+{
+  private final JsArray<E> _data;
+
+  public JsoReadOnlySet( final JsArray<E> data )
+  {
+    _data = data;
+  }
+
+  public JsArray<E> asArray()
+  {
+    return _data;
+  }
+
+  @Override
+  public Iterator<E> iterator()
+  {
+    return new JsoIterator<E>( this );
+  }
+
+  @Override
+  public int size()
+  {
+    return _data.length();
+  }
+
+  static class JsoIterator<E extends JavaScriptObject>
+    implements Iterator<E>
+  {
+    private JsoReadOnlySet<E> _set;
+    private int _offset;
+
+    JsoIterator( final JsoReadOnlySet<E> set )
+    {
+      _set = set;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+      return _offset < _set.size();
+    }
+
+    @Override
+    public E next()
+    {
+      return _set._data.get( _offset++ );
+    }
+
+    @Override
+    public void remove()
+    {
+      throw new UnsupportedOperationException();
+    }
+  }
+}
